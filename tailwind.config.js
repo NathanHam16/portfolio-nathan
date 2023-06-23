d71ac3ca205faa515759465e661e8406ffd24eec
Nathan Wang <nathan123wang@gmail.com>
2023-06-23 19:15:41 +1000

    PostCss settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 
+
 module.exports = {
   content: ['app/**/*.{js,ts,tsx}','components/**/*.{js,ts,tsx}',],
   theme: {
